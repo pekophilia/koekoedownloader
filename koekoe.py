80886a91f1d43877cbcdbdaccfe7bfd60aa3f841
eroshh <67087811+pekophilia@users.noreply.github.com>
2024-08-25 00:13:55 +0900

    Update koekoe.py

--- koekoe.py
+++ koekoe.py
@@ -7,6 +7,7 @@ from pathvalidate import sanitize_filename
 import pathlib
 import os
 import sys
+from datetime import datetime, timedelta
 
 class KoekoeException_ServerError(Exception):
     pass
@@ -44,6 +45,27 @@ def add_archive(url: str, filepath="archive.txt"):
     with open(filepath, "a") as f:
         f.write(url+"\n")
 
+def parse_postdate(date_text):
+    unit = {"日前": "days", "時間前": "hours", "分前": "minutes"}
+    old = []
+    post_date = ""
+    now = datetime.now()
+    for key, value in unit.items():
+        if key in date_text:
+            old = {value: int(date_text.replace(key, ""))}
+            post_date = now - timedelta(**old)
+            post_date = post_date.strftime(f"%Y-%m-%d")
+            break
+    else:
+        #yy/mm/dd
+        parts = date_text.split('/')
+        year = int(parts[0])
+        year += 2000 
+        month = parts[1].zfill(2)
+        day = parts[2].zfill(2)
+        post_date = "{0}-{1}-{2}".format(year,month,day) 
+    
+    return str(post_date)
 
 def get_html(url):
     retry = 2
@@ -70,12 +92,13 @@ def get_postlist(url: str, limit = 100) -> list:
 
     postlist = []
     for i in range(limit_page):
+        postlist_ = []
         print("ページ数: {0} URL: {1}".format(i+1, url))
         html = get_html(url)
         tree = html_.fromstring(html)
         
         a_tags = tree.xpath('//a[contains(@href, "detail") and contains(@title, "の投稿")]')
-
+        
         if a_tags:
             j = 0
             for a_tag in a_tags:
@@ -83,22 +106,30 @@ def get_postlist(url: str, limit = 100) -> list:
                     link = "https://koe-koe.com/" + a_tag.get("href")
                     username = a_tag.text_content()
                     title = re.sub(r"^.|.{4}$", "", a_tag.get("title")) 
-                    postlist.append({"link": link, "username": username, "title": title})
+                    postlist_.append({"link": link, "username": username, "title": title, "date": ""})
                 j += 1
         
+        p_date_tags = tree.xpath('//p[@class="meta" and contains(text(), "@")]')
+        j = 0
+        for p_tag in p_date_tags:
+            date = parse_postdate(p_tag.text_content().split("@")[-1])
+            postlist_[j]["date"] = date
+            j+=1
+
         next_link = tree.xpath('//*[@id="content"]/div[13]/div/a[2]')
-        url = ""
         if not next_link:
-            next_link = tree.xpath('//*[@id="content"]/div[14]/div/a')
-        
-        if next_link:
-            if next_link[0].attrib["href"] == "search.php":
+            next_link = tree.xpath('//*[@id="content"]/div[14]/div/a')        
+            if next_link:
+                if next_link[0].attrib["href"] == "search.php":
+                    break
+                if len(next_link) == 1 and "p=" in url:
+                    break
+            else:
                 break
-        else:
-            break
+                
         url = next_link[-1].attrib["href"]
         url = "https://koe-koe.com/" + url;
-
+        postlist += postlist_
 
     return postlist
 
@@ -174,6 +205,7 @@ def main():
             url_ = post["link"]
             username_ = post["username"]
             title_ = post["title"]
+            date = post["date"]
             id = url_.split("n=")[-1]
             dlurl = posturl_to_audiourl(url_)
             
@@ -182,7 +214,7 @@ def main():
                 print("archive.txtによりスキップされました: {0}".format(url_))
                 continue
             try:
-                filename = "save/{0}/[{1}]{2}".format(sanitize_filename(username_), id, sanitize_filename(title_))
+                filename = "save/{0}/[{1}]{2}({3})".format(sanitize_filename(username_), id, sanitize_filename(title_), date)
                 download_voice(dlurl, filename)
                 print("保存({0}/{1}): {2}".format(i+1, postlist_len, url_))
                 downloaded_list.append(url_)
